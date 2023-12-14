84e6580a7307a9da8d8c8ad489ee37cba2db8cd2
Raphael K. Dias Santos <raphaelk.diassantos.1@gmail.com>
2023-12-14 09:20:47 -0300

    Update webcam_window.py
    Updated canvas resizing

--- gui/webcam_window.py
+++ gui/webcam_window.py
@@ -1,41 +1,41 @@
 import cv2
 import tkinter as tk
 from PIL import Image, ImageTk
-
 from utils.file_management import FileManagement
 
 class WebcamWindow:
+
     def __init__(self, canvas, video_source=0):
         self.file_management = FileManagement()
-
         self.canvas = canvas
-
-        # Open the video source (webcam)
+        self.photo_image = None
         self.vid = cv2.VideoCapture(video_source)
-
-        # Call the update method to start streaming video
         self.update()
 
     def update(self):
-        # Get a frame from the video source
         ret, frame = self.vid.read()
 
         if ret:
-            # Convert the frame to RGB format
+            frame = cv2.resize(frame, (640, 480))
             rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-            # Convert the frame to a PhotoImage object
             photo = ImageTk.PhotoImage(image=Image.fromarray(rgb_frame))
 
-            # Update the canvas with the new PhotoImage
-            self.canvas.config(width=photo.width(), height=photo.height())
-            self.canvas.create_image(0, 0, anchor=tk.NW, image=photo)
-
-            # Keep a reference to avoid garbage collection
-            self.canvas.photo = photo
-
-            # Call the update method again after the specified delay
+            # Resize the image
+            canvas_width = 640
+            canvas_height = 480
+            resized_img = Image.fromarray(rgb_frame).resize((canvas_width, canvas_height))
+            resized_photo = ImageTk.PhotoImage(resized_img)
+            self.canvas.config(width=canvas_width, height=canvas_height)
+            self.canvas.create_image(340, 0, anchor="n", image=resized_photo)
+            self.canvas.photo = resized_photo
+            self.photo_image = resized_photo
             self.canvas.after(10, self.update)
+
+    def get_image_size(self):
+        if self.photo_image:
+            return self.photo_image.width(), self.photo_image.height()
+        else:
+            return 0, 0
 
     def stop_video(self):
         # Release the video source
@@ -48,15 +48,9 @@ class WebcamWindow:
 
         if ret:
             # Convert the frame to RGB format
-            #rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image_filename = "orig_" + self.file_management.get_image_filename(tag)
-
             image_path = dir_path + image_filename
-
             # Save the captured frame as an image file
-            cv2.imwrite(image_path, frame)
-
+            cv2.imwrite(image_path, rgb_frame)
             return image_path
-
-
