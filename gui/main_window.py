e21b33f0db751de12009de5e0de9c2a8de7b46d1
Raphael K. Dias Santos <raphaelk.diassantos.1@gmail.com>
2023-12-14 14:32:11 -0300

    Update main_window.py
    rewritten with clean code

--- gui/main_window.py
+++ gui/main_window.py
@@ -8,12 +8,12 @@ from utils.qr_code_reader import QRCodeReader
 from gui.webcam_window import WebcamWindow
 from PIL import Image, ImageTk
 
-# Caminho padrão do diretório de salvamento dos arquivos
+# Constants
 DEFAULT_SAVING_DIRECTORY = "/home/sitech/Documents/teste_predic/"
-sample_tag = "452156267"
-resource_path = "resources/samples/predic/test.jpg"
+SAMPLE_TAG = "452156267"
+RESOURCE_PATH = os.path.join("resources", "samples", "predic", "test.jpg")
+CANVAS_WIDTH, CANVAS_HEIGHT = 640, 480
 
-# Example usage in another application
 class MainWindow:
 
     def __init__(self, root):
@@ -22,8 +22,8 @@ class MainWindow:
         self.root.state('zoomed')
         self.root.resizable(width=False, height=False)
         self.file_management = FileManagement()
+        #self.qr_code_reader = QRCodeReader()
         self.impurity_detector = ImpurityDetector()
-        # self.qr_code_reader = QRCodeReader()
         self.result_image_canvas = None
         self.image_tk = None
         self.init_ui(self.root)
@@ -70,23 +70,19 @@ class MainWindow:
         # Create a canvas for the result image
         self.result_image_canvas = self.create_result_image_canvas(root)
 
-        # Define o tamanho desejado do canvas
-        canvas_width = 640
-        canvas_height = 480
+        # Load the image using PIL
+        img_pil = Image.open(RESOURCE_PATH)
 
-        # Carrega a imagem usando PIL
-        img_pil = Image.open(resource_path)
-
-        # Calcula o fator de zoom para preencher o canvas
-        zoom_factor = max(canvas_width / img_pil.width, canvas_height / img_pil.height)
-        # Redimensiona a imagem aplicando o fator de zoom
+        # Calculate the zoom factor to fill the canvas
+        zoom_factor = max(CANVAS_WIDTH / img_pil.width, CANVAS_HEIGHT / img_pil.height)
+        # Resize the image applying the zoom factor
         img_pil = img_pil.resize((int(img_pil.width * zoom_factor), int(img_pil.height * zoom_factor)))
 
-        # Converte a imagem para o formato compatível com Tkinter
+        # Convert the image to the format compatible with Tkinter
         self.image_tk = ImageTk.PhotoImage(img_pil)
 
-        # Exibe a imagem no Canvas result_image_canvas
-        self.result_image_canvas.config(width=canvas_width, height=canvas_height)
+        # Display the image on the Canvas result_image_canvas
+        self.result_image_canvas.config(width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
         self.result_image_canvas.create_image(340, 0, anchor="n", image=self.image_tk)
 
     def create_result_image_canvas(self, root):
@@ -115,9 +111,26 @@ class MainWindow:
         Returns:
             tk.Canvas: The created Canvas widget for displaying images.
         """
-        canvas = tk.Canvas(root, width=640, height=480, borderwidth=1, relief="solid")
+        canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT) # borderwidth=1, relief="solid"
         canvas.grid(column=column, row=row, sticky=sticky)
         return canvas
+
+    def load_and_display_result_image(self):
+        # Load the image using PIL
+        img_pil = Image.open(RESOURCE_PATH)
+
+        # Calculate the zoom factor to fill the canvas
+        zoom_factor = max(CANVAS_WIDTH / img_pil.width, CANVAS_HEIGHT / img_pil.height)
+
+        # Resize the image applying the zoom factor
+        img_pil = img_pil.resize((int(img_pil.width * zoom_factor), int(img_pil.height * zoom_factor)))
+
+        # Convert the image to the format compatible with Tkinter
+        self.image_tk = ImageTk.PhotoImage(img_pil)
+
+        # Display the image on the Canvas result_image_canvas
+        self.result_image_canvas.config(width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+        self.result_image_canvas.create_image(340, 0, anchor="n", image=self.image_tk)
 
     def configure_button_hover(self, button):
         """
@@ -161,9 +174,9 @@ class MainWindow:
         # Resultado da análise ("Amostra Contaminada" / "Amostra Livre de Impureza")
         resultLabel = tk.Label(root, text='', font=("Helvetica", 16))
         resultLabel.grid(column=0, row=3, columnspan=3)
-        sample_dir = self.file_management.make_sample_dir(sample_tag)
-        src_image_path = self.webcam_window.capture_frame(sample_dir, sample_tag)
-        result = self.impurity_detector.search_for_impurity(sample_dir, sample_tag, src_image_path)
+        sample_dir = self.file_management.make_sample_dir(SAMPLE_TAG)
+        src_image_path = self.webcam_window.capture_frame(sample_dir, SAMPLE_TAG)
+        result = self.impurity_detector.search_for_impurity(sample_dir, SAMPLE_TAG, src_image_path)
         if result:
             resultLabel.config(text="Amostra contaminada")
         else:
